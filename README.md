9c621a8f544b1f3f3e4463a24a46da66827f0e57
Group 1 <group1@not_a_real_email.com>
2023-02-10 05:11:21 -0600

    Add build commands

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# CS739-P1
-AFS-like distributed File system
+Debug build:
+cmake -S . -B debug -DCMAKE_INSTALL_PREFIX=$HOME/.local/bin -DCMAKE_BUILD_TYPE=Debug; cmake --build debug --parallel
+
+Release build:
+cmake -S . -B release -DCMAKE_INSTALL_PREFIX=$HOME/.local/bin -DCMAKE_BUILD_TYPE=Release; cmake --build release --parallel
--- grpc_setup.cmake
+++ grpc_setup.cmake
@@ -99,6 +99,7 @@ else()
 
   # Find Protobuf installation
   # Looks for protobuf-config.cmake file installed by Protobuf's cmake installation.
+  message(STATUS "Using gRPC via install location.")
   set(protobuf_MODULE_COMPATIBLE TRUE)
   find_package(Protobuf CONFIG REQUIRED)
   message(STATUS "Using protobuf ${Protobuf_VERSION}")
