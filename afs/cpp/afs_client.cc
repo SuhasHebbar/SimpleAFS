b98ff6d47b690bd5fa7c5144ea41121e2d33de6f
Ali Asgar Sabir <aliasgar@rockhopper-05.cs.wisc.edu>
2023-02-12 02:24:44 -0600

    Added implementation for other procedures except TestAuth

--- afs/cpp/afs_client.cc
+++ afs/cpp/afs_client.cc
@@ -2,6 +2,7 @@
 #include "afs.grpc.pb.h"
 #include <fstream>
 #include <cstdio>
+#include <vector>
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -13,6 +14,9 @@ using afs::FileData;
 using afs::IOResult;
 using afs::AfsService;
 using afs::StoreData;
+using afs::RenameArgs;
+using afs::DirectoryData;
+using afs::StatData;
 
 class AfsClient{
     public:
@@ -53,15 +57,11 @@ class AfsClient{
             StoreData storedata;
             Path* path =  new Path;
             path->set_name(filepath);
-            //std::cout<<path->name()<<"\n";
-            //std::cerr<<"0"<<"\n";
             storedata.set_allocated_path(path);
             FileData* data = new FileData;
             data->set_contents(filedata);
-            //std::cout<<data->contents()<<"\n";
             storedata.set_allocated_filedata(data);
             IOResult result;
-            //std::cerr<<"1"<<"\n";
             Status status = stub_->Store(&context, storedata, &result);
             if(status.ok()){
                 return result;
@@ -71,8 +71,119 @@ class AfsClient{
                 std :: cout << "Everything Sucks"<<std::endl;
                 return result;
             }
-            //std::cerr<<"2"<<"\n";
         }
+
+        IOResult Remove(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Remove(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Create(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Create(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Rename(std::string oldname, std::string newname){
+            ClientContext context;
+            Path* oldpath = new Path;
+            oldpath->set_name(oldname);
+            Path* newpath = new Path;
+            newpath->set_name(newname);
+            RenameArgs args;
+            args.set_allocated_oldname(oldpath);
+            args.set_allocated_newname(newpath);
+            IOResult result;
+            Status status = stub_->Rename(&context, args, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Makedir(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Makedir(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Removedir(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Removedir(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        std::vector<std::string> GetFileStat(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            StatData statdata;
+            Status status = stub_->GetFileStat(&context, path, &statdata);
+            std::vector<std::string> files;
+            if(status.ok()){
+                if(statdata.status().success()){
+                 int size = statdata.dd().files_size();
+                 for(int i=0;i<size;i++){
+                    files.push_back(statdata.dd().files(i));
+                 }
+                }
+                else{
+                   files.push_back(statdata.status().err_message()); 
+                }
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                files.push_back(statdata.status().err_message());
+            }
+            return files;
+        } 
+
     private:
         std::unique_ptr<AfsService::Stub> stub_;
 };
@@ -86,11 +197,15 @@ void Run() {
         )
     );
 
+    // Fetch
+    std::cout << "Fetch..." << std::endl;
     std::string response;
-
     std::string filepath = "../../README.md";
     response = client.Fetch(filepath);
     std::cout << response << std::endl;
+
+    //Store
+    std::cout << "Store..." << std::endl;
     IOResult result = client.Store("/home/aliasgar/CS739-P1/afs/cpp/hello_world.txt", "Hello World"); 
     if(result.success()){
         std::cout<<"Sucess"<<"\n";
@@ -98,6 +213,68 @@ void Run() {
     else{
         std::cout<<"Failure"<<"\n";
         std::cout<<result.err_message();
+    }
+
+    //Remove
+    std::cout << "Remove..." << std::endl;
+    result = client.Remove("/home/aliasgar/CS739-P1/afs/cpp/hello_world.txt"); 
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Create
+    std::cout << "Create..." << std::endl;
+    result = client.Create("/home/aliasgar/CS739-P1/afs/cpp/hello_wisc.txt");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Rename
+    std::cout << "Rename..." << std::endl;
+    result = client.Rename("/home/aliasgar/CS739-P1/afs/cpp/hello_wisc.txt", "/home/aliasgar/CS739-P1/afs/cpp/hello_uw.txt");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Makedir
+    std::cout << "Makedir..." << std::endl;
+    result = client.Makedir("/home/aliasgar/CS739-P1/afs/cpp/temp");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Removedir
+    std::cout << "Removedir..." << std::endl;
+    result = client.Removedir("/home/aliasgar/CS739-P1/afs/cpp/temp");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    // GetFileStat
+    std::cout << "GetFileStat..." << std::endl;
+    std::vector<std::string> files = client.GetFileStat("/home/aliasgar/CS739-P1/afs/cpp");
+    for(int i=0;i<files.size();i++){
+        std::cout<<files[i]<<std::endl;
     }
 }
 
--- afs/cpp/afs_server.cc
+++ afs/cpp/afs_server.cc
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <dirent.h>
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -20,6 +21,9 @@ using afs::FileData;
 using afs::IOResult;
 using afs::AfsService;
 using afs::StoreData;
+using afs::RenameArgs;
+using afs::DirectoryData;
+using afs::StatData;
 
 class AfsServiceImpl final : public AfsService::Service{
     Status Fetch(ServerContext* context, const Path* path, ServerWriter<FileData>* writer){
@@ -59,7 +63,7 @@ class AfsServiceImpl final : public AfsService::Service{
 
     Status Store(ServerContext* context, const StoreData* storedata, IOResult* result){
         std::string targetPath = (storedata->path()).name();
-        int targetfd = open(targetPath.c_str(), O_RDWR | O_CREAT, S_IRWXU);
+        int targetfd = open(targetPath.c_str(), O_RDWR | O_CREAT, 00664);
         if(targetfd == -1){
             result->set_success(false);
             result->set_err_message("Error while creating or opening target file");
@@ -69,11 +73,92 @@ class AfsServiceImpl final : public AfsService::Service{
         if(write(targetfd, &data[0], data.size()) == -1){
             result->set_success(false);
             result->set_err_message("Error while writing to target file");
+            close(targetfd);
             return Status::OK;
         }
         close(targetfd);
         result->set_success(true);
         return Status::OK;     
+    }
+
+    Status Remove(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(unlink(pathname.c_str()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while removing file");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;  
+    }
+
+    Status Create(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(creat(pathname.c_str(), 00664) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while creating file");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;  
+    }
+
+    Status Rename(ServerContext* context, const RenameArgs* args, IOResult* result){
+        std::string oldpath = (args->oldname()).name();
+        std::string newpath = (args->newname()).name();
+        if(rename(oldpath.c_str(), newpath.c_str()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while renaming file");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;
+    }
+
+    Status Makedir(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(mkdir(pathname.c_str(), 0664) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while creating directory");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;
+    }
+
+    Status Removedir(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(rmdir(pathname.c_str()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while removing directory");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;
+    }
+
+    Status GetFileStat(ServerContext* context, const Path* path, StatData* statdata ){
+        std::string pathname = path->name();
+        struct dirent* dentry;
+        
+        DIR* dr = opendir(pathname.c_str());
+        if(dr == NULL){
+            IOResult* result = new IOResult;
+            result->set_success(false);
+            result->set_err_message("Error while opening directory");
+            statdata->set_allocated_status(result);
+            return Status::OK;
+        }
+        DirectoryData* dd = new DirectoryData;
+        while((dentry = readdir(dr)) != NULL){
+            dd->add_files(dentry->d_name);
+        }
+        statdata->set_allocated_dd(dd);
+        closedir(dr);
+        IOResult* result = new IOResult;
+        result->set_success(true);
+        statdata->set_allocated_status(result);
+        return Status::OK;
     }
 };
 
