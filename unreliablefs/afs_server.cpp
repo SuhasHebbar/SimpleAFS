c7e28c2ed89bc6d3ef7b54cabfc46307d68be7c2
Ali Asgar Sabir <aliasgarsabir@MacBook-Air.attlocal.net>
2023-02-12 10:07:38 -0600

    Add implementation for Store and other procedures except TestAuth

--- unreliablefs/afs_server.cpp
+++ unreliablefs/afs_server.cpp
@@ -4,6 +4,12 @@
 #include <fstream>
 #include <iostream>
 #include <cstdio>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -14,10 +20,14 @@ using afs::Path;
 using afs::FileData;
 using afs::IOResult;
 using afs::AfsService;
+using afs::StoreData;
+using afs::RenameArgs;
+using afs::DirectoryData;
+using afs::StatData;
 
 class AfsServiceImpl final : public AfsService::Service{
-    Status Fetch(ServerContext* context, const Path* filepath, ServerWriter<FileData>* writer){
-        std::string pathname = filepath->path();
+    Status Fetch(ServerContext* context, const Path* path, ServerWriter<FileData>* writer){
+        std::string pathname = path->name();
         int block_size = 8000;
         FILE* fp;
         fp = fopen(pathname.c_str(), "r");
@@ -48,6 +58,106 @@ class AfsServiceImpl final : public AfsService::Service{
             writer->Write(filedata);
         }
         fclose(fp);
+        return Status::OK;
+    }
+
+    Status Store(ServerContext* context, const StoreData* storedata, IOResult* result){
+        std::string targetPath = (storedata->path()).name();
+        int targetfd = open(targetPath.c_str(), O_RDWR | O_CREAT, 00664);
+        if(targetfd == -1){
+            result->set_success(false);
+            result->set_err_message("Error while creating or opening target file");
+            return Status::OK;
+        }
+        std::string data = (storedata->filedata()).contents();
+        if(write(targetfd, &data[0], data.size()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while writing to target file");
+            close(targetfd);
+            return Status::OK;
+        }
+        close(targetfd);
+        result->set_success(true);
+        return Status::OK;     
+    }
+
+    Status Remove(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(unlink(pathname.c_str()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while removing file");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;  
+    }
+
+    Status Create(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(creat(pathname.c_str(), 00664) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while creating file");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;  
+    }
+
+    Status Rename(ServerContext* context, const RenameArgs* args, IOResult* result){
+        std::string oldpath = (args->oldname()).name();
+        std::string newpath = (args->newname()).name();
+        if(rename(oldpath.c_str(), newpath.c_str()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while renaming file");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;
+    }
+
+    Status Makedir(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(mkdir(pathname.c_str(), 0664) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while creating directory");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;
+    }
+
+    Status Removedir(ServerContext* context, const Path* path, IOResult* result){
+        std::string pathname = path->name();
+        if(rmdir(pathname.c_str()) == -1){
+            result->set_success(false);
+            result->set_err_message("Error while removing directory");
+            return Status::OK;
+        }
+        result->set_success(true);
+        return Status::OK;
+    }
+
+    Status GetFileStat(ServerContext* context, const Path* path, StatData* statdata ){
+        std::string pathname = path->name();
+        struct dirent* dentry;
+        
+        DIR* dr = opendir(pathname.c_str());
+        if(dr == NULL){
+            IOResult* result = new IOResult;
+            result->set_success(false);
+            result->set_err_message("Error while opening directory");
+            statdata->set_allocated_status(result);
+            return Status::OK;
+        }
+        DirectoryData* dd = new DirectoryData;
+        while((dentry = readdir(dr)) != NULL){
+            dd->add_files(dentry->d_name);
+        }
+        statdata->set_allocated_dd(dd);
+        closedir(dr);
+        IOResult* result = new IOResult;
+        result->set_success(true);
+        statdata->set_allocated_status(result);
         return Status::OK;
     }
 };
