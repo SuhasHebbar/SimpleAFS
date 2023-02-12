578f227cc07f592b401592c80fe52dc4c0d9cdc1
Ali Asgar Sabir <aliasgarsabir@MacBook-Air.attlocal.net>
2023-02-12 10:07:38 -0600

    Add implementation for Store and other procedures except TestAuth

--- unreliablefs/afs_client.cpp
+++ unreliablefs/afs_client.cpp
@@ -2,10 +2,7 @@
 #include "afs.grpc.pb.h"
 #include <fstream>
 #include <cstdio>
-#include <memory>
-
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <vector>
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -16,6 +13,10 @@ using afs::Path;
 using afs::FileData;
 using afs::IOResult;
 using afs::AfsService;
+using afs::StoreData;
+using afs::RenameArgs;
+using afs::DirectoryData;
+using afs::StatData;
 
 class AfsClient{
     public:
@@ -23,7 +24,7 @@ class AfsClient{
 
         std::string Fetch(std::string filepath){
             Path path;
-            path.set_path(filepath);
+            path.set_name(filepath);
             FileData data;
             ClientContext context;
 
@@ -50,6 +51,139 @@ class AfsClient{
                 return "Everything Sucks";
             }
         }
+
+        IOResult Store(std::string filepath, std::string filedata){
+            ClientContext context;
+            StoreData storedata;
+            Path* path =  new Path;
+            path->set_name(filepath);
+            storedata.set_allocated_path(path);
+            FileData* data = new FileData;
+            data->set_contents(filedata);
+            storedata.set_allocated_filedata(data);
+            IOResult result;
+            Status status = stub_->Store(&context, storedata, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Remove(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Remove(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Create(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Create(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Rename(std::string oldname, std::string newname){
+            ClientContext context;
+            Path* oldpath = new Path;
+            oldpath->set_name(oldname);
+            Path* newpath = new Path;
+            newpath->set_name(newname);
+            RenameArgs args;
+            args.set_allocated_oldname(oldpath);
+            args.set_allocated_newname(newpath);
+            IOResult result;
+            Status status = stub_->Rename(&context, args, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Makedir(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Makedir(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        IOResult Removedir(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            IOResult result;
+            Status status = stub_->Removedir(&context, path, &result);
+            if(status.ok()){
+                return result;
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                return result;
+            }
+        }
+
+        std::vector<std::string> GetFileStat(std::string filepath){
+            ClientContext context;
+            Path path;
+            path.set_name(filepath);
+            StatData statdata;
+            Status status = stub_->GetFileStat(&context, path, &statdata);
+            std::vector<std::string> files;
+            if(status.ok()){
+                if(statdata.status().success()){
+                 int size = statdata.dd().files_size();
+                 for(int i=0;i<size;i++){
+                    files.push_back(statdata.dd().files(i));
+                 }
+                }
+                else{
+                   files.push_back(statdata.status().err_message()); 
+                }
+            }
+            else{
+                std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+                std :: cout << "Everything Sucks"<<std::endl;
+                files.push_back(statdata.status().err_message());
+            }
+            return files;
+        } 
+
     private:
         std::unique_ptr<AfsService::Stub> stub_;
 };
@@ -63,13 +197,86 @@ void Run() {
         )
     );
 
+    // Fetch
+    std::cout << "Fetch..." << std::endl;
     std::string response;
-
-    std::string filepath = "../README.md";
+    std::string filepath = "../../README.md";
     response = client.Fetch(filepath);
     std::cout << response << std::endl;
-}
 
+    //Store
+    std::cout << "Store..." << std::endl;
+    IOResult result = client.Store("/home/aliasgar/CS739-P1/afs/cpp/hello_world.txt", "Hello World"); 
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Remove
+    std::cout << "Remove..." << std::endl;
+    result = client.Remove("/home/aliasgar/CS739-P1/afs/cpp/hello_world.txt"); 
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Create
+    std::cout << "Create..." << std::endl;
+    result = client.Create("/home/aliasgar/CS739-P1/afs/cpp/hello_wisc.txt");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Rename
+    std::cout << "Rename..." << std::endl;
+    result = client.Rename("/home/aliasgar/CS739-P1/afs/cpp/hello_wisc.txt", "/home/aliasgar/CS739-P1/afs/cpp/hello_uw.txt");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Makedir
+    std::cout << "Makedir..." << std::endl;
+    result = client.Makedir("/home/aliasgar/CS739-P1/afs/cpp/temp");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    //Removedir
+    std::cout << "Removedir..." << std::endl;
+    result = client.Removedir("/home/aliasgar/CS739-P1/afs/cpp/temp");
+    if(result.success()){
+        std::cout<<"Sucess"<<"\n";
+    }
+    else{
+        std::cout<<"Failure"<<"\n";
+        std::cout<<result.err_message();
+    }
+
+    // GetFileStat
+    std::cout << "GetFileStat..." << std::endl;
+    std::vector<std::string> files = client.GetFileStat("/home/aliasgar/CS739-P1/afs/cpp");
+    for(int i=0;i<files.size();i++){
+        std::cout<<files[i]<<std::endl;
+    }
+}
 
 extern "C" {
     void test_afs() {
