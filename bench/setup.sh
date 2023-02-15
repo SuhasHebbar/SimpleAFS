a73616286c0c4d8f03f08ea6102a9624cd4c69f2
Suhas <hebbar2@wisc.edu>
2023-02-15 17:20:07 -0600

    Add setup scripts

---
+++ bench/setup.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+echo Starting setup script
+
+chmod 400 ~/.ssh/id_ed25519*
+
+# Disable ASLR for filebench
+# Value was 2 previously
+sudo bash -c "echo 0 > /proc/sys/kernel/randomize_va_space"
+
+# sudo apt update
+sudo apt install -y htop fuse libfuse-dev build-essential autoconf libtool pkg-config cmake qemu flex bison python-is-python3 python3-paramiko
+
+if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
+	echo Adding \$HOME/bin to path
+	mkdir -p ~/bin
+	echo "export PATH=\$HOME/bin:\$PATH" >> ~/.bashrc
+fi
+
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+	echo Adding \$HOME/.local/bin to path
+	mkdir -p ~/bin
+	echo "export PATH=\$HOME/.local/bin:\$PATH" >> ~/.bashrc
+fi
+
+if [[ "x" == "x$(ls -ald fs)" ]]; then
+	git clone git@github.com:SuhasHebbar/CS739-P1.git fs
+fi
+
+# if [[ ":$PATH:" != *":$HOME/go/bin:"* ]]; then
+# 	echo Adding \$HOME/go/	echo Adding \$HOME/bin to pathbin to path
+# 	mkdir -p ~/go/bin
+# 	echo "export PATH=\$HOME/go/bin:\$PATH" >> ~/.bashrc
+# fi
+
+# if [[ "x" == "x$(which protoc-gen-go)" || "x" == "x$(which protoc-gen-go-grpc)" ]]; then
+# 	go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+# 	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+# fi
+
+
+if [[ "x" == "x$(which nvim)" ]]; then
+	curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/bin/nvim
+	chmod u+x ~/bin/nvim
+fi
+
+if [[ "x" == "x$(ls -ald ~/filebench)" ]]; then
+	# We're using the alpha version because the latest stable version does not have the ./configure script.
+	curl -L https://github.com/filebench/filebench/releases/download/1.5-alpha3/filebench-1.5-alpha3.zip -o ~/filebench.zip
+	unzip filebench.zip
+	mv filebench-1.5-alpha3 filebench
+	rm -rf filebench.zip
+	pushd ~/filebench
+	./configure --prefix=$HOME/.local
+	make
+	make install
+	popd
+fi
+
+if [[ "x" == "x$(ls -ald grpc)" ]]; then
+	MY_INSTALL_DIR=~/.local/bin
+	mkdir -p $MY_INSTALL_DIR
+	git clone --recurse-submodules -b v1.50.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
+	pushd grpc
+	mkdir -p cmake/build
+	pushd cmake/build
+	cmake -DgRPC_INSTALL=ON \
+	      -DgRPC_BUILD_TESTS=OFF \
+	      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+	      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+	      ../..
+	make -j 20
+	make install
+	popd
+	popd
+fi
+
+if [[ "x" == "x$(ls -ald bench)" ]]; then
+	curl -L https://github.com/jingliu9/unreliablefs/archive/refs/heads/cs739.zip -O
+	unzip cs739.zip
+	rm -rf cs739.zip
+	mv unreliablefs-cs739 bench
+fi
+
+if [[ "x" == "x$(ls -ald xv6-public)" ]]; then
+	git clone https://github.com/mit-pdos/xv6-public.git
+fi
+
+
+if [[ "x" == "x$(ls -ald leveldb)" ]]; then
+	git clone https://github.com/google/leveldb.git
+	pushd leveldb
+	git submodule update --init
+	mkdir build
+	pushd build
+	cmake ..
+	make clean
+	popd
+	popd
+fi
+
+rm -rf ~/.config/nvim
+rm -rf ~/hebbar2_config
+
+git clone https://github.com/SuhasHebbar/config hebbar2_config
+ln -s ~/hebbar2_config/nvim ~/.config/nvim
+
+git config --global user.name "Group 1"
+git config --global user.email "group1@not_a_real_email.com"
+
+# sudo apt remove --purge -y neovim clangd cmake
+# rm -rvf ~/grpc
+echo Finished setup script
