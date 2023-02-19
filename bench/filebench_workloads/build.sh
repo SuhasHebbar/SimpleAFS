fce075d110a442ba0c4874db2bcdaa9b7f51550c
Group 1 <group1@not_a_real_email.com>
2023-02-19 12:33:26 -0600

    Timing code

--- bench/filebench_workloads/build.sh
+++ bench/filebench_workloads/build.sh
@@ -10,32 +10,54 @@ rm -rf $ubase/mnt/*
 
 $ubase/build/unreliablefs/unreliablefs $ubase/mnt -basedir=$ubase/base -d 2>stderr.log >stdout.log & 
 pid=$!
-
 sleep 10
 
 echo the executable is $ubase/build/unreliablefs/unreliablefs
 
-cd $ubase/mnt
+function xv6_bench {
+	cd $ubase/mnt
+	git clone https://github.com/mit-pdos/xv6-public.git
+	cd xv6-public
 
-echo Starting xv6
-git clone https://github.com/mit-pdos/xv6-public.git
-cd xv6-public
-make qemu-nox
-echo Ending xv6
+	# echo Ctrl + a and the x. Source: https://superuser.com/questions/1543533/how-can-i-simulate-keystrokes-in-bash-without-any-packages
+	echo $'\cax' | make qemu-nox
+} 
 
-cd $ubase/mnt
+function leveldb_bench_setup {
+	cd $ubase/mnt
 
-echo Starting leveldb
-git clone https://github.com/google/leveldb.git
-cd leveldb
-git submodule update --init
-mkdir build
-cd build
-cmake ..
-make clean
-echo Ending leveldb
+	git clone https://github.com/google/leveldb.git
+	cd leveldb
+	git submodule update --init
+	mkdir build
+	cd build
+	cmake ..
+}
 
-make -j 10
+function leveldb_bench {
+	cd $ubase/mnt
+	make clean
+	make -j "$1"
+	make clean
+}
+
+rm -rf $ubase/mnt/xv6-public
+echo Starting xv6_bench
+time ( xv6_bench > /dev/null 2>&1)
+echo Ending xv6_bench
+
+ts=(1 2 4 8 16)
+rm -rf $ubase/mnt/leveldb
+
+echo Starting leveldb_bench_setup
+time ( leveldb_bench_setup > /dev/null 2>&1 )
+echo Ending leveldb_bench_setup
+
+for i in "${ts[@]}"; do
+	echo Starting leveldb_bench $i
+	time ( leveldb_bench $i > /dev/null 2>&1 )
+	echo Ending leveldb_bench $i
+done
 
 kill $pid
 pkill unreliablefs
