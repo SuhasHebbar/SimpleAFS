5c200e25f39700008345e51b5e5e028489611dea
Group 1 <group1@not_a_real_email.com>
2023-02-24 21:04:58 -0600

    minor changes

--- unreliablefs/common.h
+++ unreliablefs/common.h
@@ -1,11 +1,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#ifdef NDEBUG
+#define D(x)
+#else
+#define D(x) x
+#endif
+
 static void DEBUG(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-#ifdef NDEBUG
-  vprintf(fmt, args);
-#endif
+  D(vprintf(fmt, args));
   va_end(args);
 }
